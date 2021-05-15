8504bcef29781fe9417012e9a0c773be958cdc16
Marcos <marcos.rodriguez.romero@hotmail.com>
2021-05-15 20:47:00 +0200

    codigo

--- src/operaciones.py
+++ src/operaciones.py
@@ -9,3 +9,12 @@ def multiplicacion(a,b):
 
 def division(a,b):
     return a / b
+    
+    
+a = float(input("Introduce el primer número: "))
+b = float(input("Introduce el segundo número: "))
+
+print("{} + {} = {}\n".format(a, b, suma(a,b)))
+print("{} - {} = {}\n".format(a, b, resta(a,b)))
+print("{} * {} = {}\n".format(a, b, multiplicacion(a,b)))
+print("{} / {} = {}".format(a, b, division(a,b)))
