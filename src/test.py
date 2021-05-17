90fd27f2a989e1a076905d03c102d42352cdc4c7
Marcos <marcos.rodriguez.romero@hotmail.com>
2021-05-17 18:26:02 +0200

    prueba

--- src/test.py
+++ src/test.py
@@ -31,5 +31,8 @@ class TestOperaciones(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'),
+        # these make sure that some options that are not applicable
+        # remain hidden from the help menu.
+        failfast=False, buffer=False, catchbreak=False)
 
