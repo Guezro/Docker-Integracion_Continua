f0b839a6a2c2354c7de2e939968b93bc61a89068
Marcos <marcos.rodriguez.romero@hotmail.com>
2021-05-18 15:50:20 +0200

    prueba

--- src/test.py
+++ src/test.py
@@ -32,7 +32,7 @@ class TestOperaciones(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'),
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='/var/jenkins_home/cker_Integraci_n_Continua_master/test-reports'),
         # these make sure that some options that are not applicable
         # remain hidden from the help menu.
         failfast=False, buffer=False, catchbreak=False)
